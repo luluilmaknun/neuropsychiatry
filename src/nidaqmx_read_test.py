274191c5b02999cb96a7228f1bff38e657e0367a
Anthony Dewa Priyasembada <anthonydewa67@gmail.com>
2020-07-20 10:40:01 +0700

    Fix pylint code smell

--- src/nidaqmx_read_test.py
+++ src/nidaqmx_read_test.py
@@ -1,16 +1,33 @@
+"""
+Test input
+"""
 import tkinter as tk
 import nidaqmx
 
 master = tk.Tk()
 master.minsize(width=400, height=400)
-w = tk.Label(master) #shows as text in the window
-w.pack() #organizes widgets in blocks before placing them in the parent.   
+w = tk.Label(master)
+w.pack()
 
 def run():
+    """
+    function to read
+    """
     with nidaqmx.Task() as task:
-        task.ai_channels.add_ai_voltage_chan("Dev1/ai0", 0, nidaqmx.constants.TerminalConfiguration.DIFFERENTIAL, -10, 10, nidaqmx.constants.VoltageUnits.VOLTS, 0)
-        task.timing.cfg_samp_clk_timing(5000, 0, nidaqmx.constants.Edge.RISING, nidaqmx.constants.AcquisitionType.CONTINUOUS, 0)
-        task.in_stream.over_write=nidaqmx.constants.OverwriteMode.OVERWRITE_UNREAD_SAMPLES
+        task.ai_channels.add_ai_voltage_chan(
+            "Dev1/ai0",
+            0,
+            nidaqmx.constants.TerminalConfiguration.DIFFERENTIAL,
+            -10, 10,
+            nidaqmx.constants.VoltageUnits.VOLTS,
+            0)
+        task.timing.cfg_samp_clk_timing(
+            5000,
+            0,
+            nidaqmx.constants.Edge.RISING,
+            nidaqmx.constants.AcquisitionType.CONTINUOUS,
+            0)
+        task.in_stream.over_write = nidaqmx.constants.OverwriteMode.OVERWRITE_UNREAD_SAMPLES
         print('1 Channel 1 Sample Read: ')
         data = task.read()
         w['text'] = '%.2f' % data #said sensor value
