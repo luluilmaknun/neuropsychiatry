0bd2182f5100bfa6537d17f6116f6f102eaf6c19
Anthony Dewa Priyasembada <anthonydewa67@gmail.com>
2020-07-21 10:52:00 +0700

    Fix linting issues

--- src/nidaqmx_read_test.py
+++ src/nidaqmx_read_test.py
@@ -8,7 +8,7 @@ master = tk.Tk()
 master.minsize(width=400, height=400)
 w = tk.Label(master)
 w.pack()
-task = nidaqmx.task.Task()
+task = nidaqmx.task.Task("ReadVoltageTwoChannel")
 task.ai_channels.add_ai_voltage_chan(
     "Dev1/ai0, Dev1/ai4",
     0,
@@ -22,8 +22,8 @@ task.timing.cfg_samp_clk_timing(
     nidaqmx.constants.Edge.RISING,
     nidaqmx.constants.AcquisitionType.CONTINUOUS,
     0)
-constant_overwrite = nidaqmx.constants.OverwriteMode
-task.in_stream.over_write = constant_overwrite.OVERWRITE_UNREAD_SAMPLES
+ConstantOverwrite = nidaqmx.constants.OverwriteMode
+task.in_stream.over_write = ConstantOverwrite.OVERWRITE_UNREAD_SAMPLES
 task.start()
 
 
@@ -31,12 +31,12 @@ def run():
     """
     function to read
     """
-    global task
     data = task.read(100)
     data_avg_chan1 = sum(data[0])/100
     data_avg_chan2 = sum(data[1])/100
-    w['text'] = '%.5f, %.5f' % (data_avg_chan1, data_avg_chan2)
+    w['text'] = 'ai0: %.5f\nai4: %.5f' % (data_avg_chan1, data_avg_chan2)
     master.after(10, run)
+
 
 run()
 master.mainloop()
