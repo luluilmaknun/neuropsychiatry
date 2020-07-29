d5e59ff61b914ab94392e28cb8bb0de44712c65e
Anthony Dewa Priyasembada <anthonydewa67@gmail.com>
2020-07-29 10:19:48 +0700

    Add constant file

--- src/main.py
+++ src/main.py
@@ -3,9 +3,10 @@ Test input
 """
 import tkinter as tk
 import nidaqmx
+import constants
 
 master = tk.Tk()
-master.minsize(width=400, height=400)
+master.minsize(width=constants.WINDOW_WIDTH, height=constants.WINDOW_LENGTH)
 w = tk.Label(master)
 w.pack()
 task = nidaqmx.task.Task("ReadVoltageTwoChannel")
@@ -13,11 +14,12 @@ task.ai_channels.add_ai_voltage_chan(
     "Dev1/ai0, Dev1/ai4",
     0,
     nidaqmx.constants.TerminalConfiguration.DIFFERENTIAL,
-    -10, 10,
+    constants.READ_DATA_MIN_VALUE,
+    constants.READ_DATA_MAX_VALUE,
     nidaqmx.constants.VoltageUnits.VOLTS,
     0)
 task.timing.cfg_samp_clk_timing(
-    5000,
+    constants.READ_DATA_SAMPLE_RATE,
     0,
     nidaqmx.constants.Edge.RISING,
     nidaqmx.constants.AcquisitionType.CONTINUOUS,
@@ -31,11 +33,11 @@ def run():
     """
     function to read
     """
-    data = task.read(100)
-    data_avg_chan1 = sum(data[0])/100
-    data_avg_chan2 = sum(data[1])/100
+    data = task.read(constants.READ_SAMPLE_PER_WINDOW_REFRESH)
+    data_avg_chan1 = sum(data[0])/constants.READ_SAMPLE_PER_WINDOW_REFRESH
+    data_avg_chan2 = sum(data[1])/constants.READ_SAMPLE_PER_WINDOW_REFRESH
     w['text'] = 'ai0: %.5f\nai4: %.5f' % (data_avg_chan1, data_avg_chan2)
-    master.after(10, run)
+    master.after(constants.WINDOW_REFRESH_TIME, run)
 
 
 run()
